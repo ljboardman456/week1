0947c2831f4a1e4be93c288deb99a5bb1d266f86
levi boardman <leviboardman456@gmail.com>
2025-01-16 13:47:43 -0600

    gfsfg

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,6 +10,8 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Welcome to ITSE1302");
+            Console.ReadLine();
         }
     }
 }
